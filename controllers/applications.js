aa497a8f3b3eb193d020d2189c0a6a9bc2e1734a
Haroon <you@example.com>
2024-07-24 15:02:20 +0300

    build-the-applications-index-page

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const User = require('../models/user')
 
-router.get('/', (req, res) => {
-    res.send('Hello applications index route!');
+router.get('/', async (req, res) => {
+    try {
+      res.render('applications/index.ejs');
+    } catch (error) {
+      console.log(error)
+      res.redirect('/')
+    }
   });
-
+  
 module.exports = router;
